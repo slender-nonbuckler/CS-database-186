5f57a3f4eb90c76d053c78ca02f7eb6cdf23dfb4
QJC <qjichun@gmail.com>
2025-05-18 18:41:37 -0700

    Initial commit of sp25-proj1

---
+++ sp25-proj1/proj1.sql
@@ -0,0 +1,141 @@
+-- Before running drop any existing views
+DROP VIEW IF EXISTS q0;
+DROP VIEW IF EXISTS q1i;
+DROP VIEW IF EXISTS q1ii;
+DROP VIEW IF EXISTS q1iii;
+DROP VIEW IF EXISTS q1iv;
+DROP VIEW IF EXISTS q2i;
+DROP VIEW IF EXISTS q2ii;
+DROP VIEW IF EXISTS q2iii;
+DROP VIEW IF EXISTS q3i;
+DROP VIEW IF EXISTS q3ii;
+DROP VIEW IF EXISTS q3iii;
+DROP VIEW IF EXISTS q4i;
+DROP VIEW IF EXISTS q4ii;
+DROP VIEW IF EXISTS q4iii;
+DROP VIEW IF EXISTS q4iv;
+DROP VIEW IF EXISTS q4v;
+
+-- Question 0
+CREATE VIEW q0(era)
+AS
+  SELECT MAX(era)
+  FROM pitching
+;
+
+-- Question 1i
+CREATE VIEW q1i(namefirst, namelast, birthyear)
+AS
+  SELECT namefirst, namelast, birthyear
+  FROM people
+  WHERE weight > 300
+;
+
+-- Question 1ii
+CREATE VIEW q1ii(namefirst, namelast, birthyear)
+AS
+  SELECT namefirst, namelast, birthyear
+  FROM people
+  WHERE namefirst LIKE "% %"
+  ORDER BY namefirst, namelast
+;
+
+-- Question 1iii
+CREATE VIEW q1iii(birthyear, avgheight, count)
+AS
+  SELECT birthyear,AVG(height), COUNT(*)
+  FROM people
+  GROUP BY birthyear
+  ORDER BY birthyear
+;
+
+-- Question 1iv
+CREATE VIEW q1iv(birthyear, avgheight, count)
+AS
+  SELECT birthyear,AVG(height), COUNT(*)
+    FROM people
+    GROUP BY birthyear
+    HAVING AVG(height) > 70
+    ORDER BY birthyear
+;
+
+-- Question 2i
+CREATE VIEW q2i(namefirst, namelast, playerid, yearid)
+AS
+  SELECT p.nameFirst, p.nameLast, p.playerID, h.yearid
+  FROM people p INNER JOIN halloffame h ON p.playerID = h.playerID
+  WHERE h.inducted = 'Y'
+  ORDER BY h.yearid DESC, h.playerID
+;
+
+-- Question 2ii
+CREATE VIEW q2ii(namefirst, namelast, playerid, schoolid, yearid)
+AS
+  SELECT p.namefirst, p.namelast, p.playerid, cp.schoolid, h.yearid
+  FROM people p
+  INNER JOIN halloffame h ON p.playerID = h.playerID
+  INNER JOIN collegeplaying cp ON p.playerID = cp.playerID
+  INNER JOIN schools s ON cp.schoolID = s.schoolID
+  WHERE h.inducted = 'Y'
+    AND s.schoolState = 'CA'
+  ORDER BY h.yearid DESC, cp.schoolid ASC,h.playerid ASC
+;
+
+-- Question 2iii
+CREATE VIEW q2iii(playerid, namefirst, namelast, schoolid)
+AS
+  SELECT p.playerid, p.namefirst, p.namelast, cp.schoolid
+  FROM people p
+  INNER JOIN halloffame h ON p.playerID = h.playerID
+  LEFT JOIN collegeplaying cp ON p.playerID = cp.playerID
+  WHERE h.inducted = 'Y'
+  ORDER BY p.playerid DESC, cp.schoolid ASC
+
+;
+
+-- Question 3i
+CREATE VIEW q3i(playerid, namefirst, namelast, yearid, slg)
+AS
+  SELECT 1, 1, 1, 1, 1 -- replace this line
+;
+
+-- Question 3ii
+CREATE VIEW q3ii(playerid, namefirst, namelast, lslg)
+AS
+  SELECT 1, 1, 1, 1 -- replace this line
+;
+
+-- Question 3iii
+CREATE VIEW q3iii(namefirst, namelast, lslg)
+AS
+  SELECT 1, 1, 1 -- replace this line
+;
+
+-- Question 4i
+CREATE VIEW q4i(yearid, min, max, avg)
+AS
+  SELECT 1, 1, 1, 1 -- replace this line
+;
+
+-- Question 4ii
+CREATE VIEW q4ii(binid, low, high, count)
+AS
+  SELECT 1, 1, 1, 1 -- replace this line
+;
+
+-- Question 4iii
+CREATE VIEW q4iii(yearid, mindiff, maxdiff, avgdiff)
+AS
+  SELECT 1, 1, 1, 1 -- replace this line
+;
+
+-- Question 4iv
+CREATE VIEW q4iv(playerid, namefirst, namelast, salary, yearid)
+AS
+  SELECT 1, 1, 1, 1, 1 -- replace this line
+;
+-- Question 4v
+CREATE VIEW q4v(team, diffAvg) AS
+  SELECT 1, 1 -- replace this line
+;
+
